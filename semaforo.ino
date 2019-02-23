85025f4ec1702c64ab559c3233482e58b01abf3e
terceiro.com.br <andre@terceiro.com.br>
2019-02-23 13:07:46 -0300

    Add files via upload

---
+++ semaforo.ino
@@ -0,0 +1,30 @@
+const int r = 13;
+const int y = 12; 
+const int g = 11; 
+const int terra = 10;
+
+void setup() {
+   pinMode(r, OUTPUT);
+   pinMode(y, OUTPUT);
+   pinMode(g, OUTPUT);
+   pinMode(terra, OUTPUT);
+
+   digitalWrite(r, LOW);
+   digitalWrite(y, LOW);
+   digitalWrite(g, LOW);
+   digitalWrite(terra, LOW);
+}       
+
+void loop() {
+  digitalWrite(g, LOW);
+  digitalWrite(r, HIGH);
+  delay(3000);
+  
+  digitalWrite(r, LOW);
+  digitalWrite(y, HIGH);
+  delay(1000);
+
+  digitalWrite(y, LOW);
+  digitalWrite(g, HIGH);
+  delay(3000);
+}
